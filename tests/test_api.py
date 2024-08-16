8c1be5f6762dbf1f8df5d304e2847480048b1edf
manu <arscaldgral@gmail.com>
2024-08-16 18:45:35 +0200

    test route dates

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,39 +1,10 @@
 import unittest
 from test_data_animov import app
-import json
-# from app import db
-# from werkzeug.security import generate_password_hash
-# from models import User
 
 class ApiTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    # def setUp(self):
-    #     self.app = app.test_client()
-    #     with app.app_context():
-    #         db.create_all()  # Crée toutes les tables
-
-    #     # Supprimer l'utilisateur si nécessaire
-    #     user = User.query.filter_by(username='testuser').first()
-    #     if user:
-    #         db.session.delete(user)
-    #         db.session.commit()
-
-    #     # Ajouter un utilisateur de test
-    #     hashed_password = generate_password_hash('password', method='sha256')
-    #     user = User(username='testuser', password=hashed_password)
-    #     db.session.add(user)
-    #     db.session.commit()
-
-    #     # Authentification et récupération du token
-    #     response = self.app.post('/login', json={'username': 'testuser', 'password': 'password'})
-    #     self.token = response.get_json().get('token', None)
-
-    # def tearDown(self):
-    #     with app.app_context():
-    #         db.session.remove()
-    #         db.drop_all()  # Supprime toutes les tables après les tests
 
     def test_get_data_animov_ch(self):
         # response = self.app.get('/get_data_animov_ch', headers={'x-access-tokens': self.token}, query_string={
@@ -45,6 +16,21 @@ class ApiTestCase(unittest.TestCase):
             'with_global_stats': 'True'
         })
         self.assertEqual(response.status_code, 200)
+
+    #  def test_dates_route(self):
+    #     # Utiliser un token valide pour les tests
+    #     token = 'votre_token_valide_ici'  # Remplacez par un token valide
+    #     headers = {'x-access-tokens': token}
+
+    #     response = self.app.get('/dates', headers=headers)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertIsInstance(json.loads(response.data), list)  # Vérifiez que la réponse est une liste
+    #     self.assertTrue(len(json.loads(response.data)) > 0)  # Vérifiez que la liste n'est pas vide
+
+    def test_dates_route_without_token(self):
+        response = self.app.get('/dates')
+        self.assertEqual(response.status_code, 401)  # Vérifie que l'accès sans token retourne un statut 401
+        self.assertIn(b'Token manquant', response.data)  # Vérifie que le message 'Token manquant' est dans la réponse
 
 if __name__ == '__main__':
     unittest.main()
