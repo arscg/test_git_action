76e3cb082ebe8932f8a94166094e2df0ee1bf40e
manu <arscaldgral@gmail.com>
2024-08-16 17:02:57 +0200

    add test route get_data_animov_ch

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 12 19:31:34 2024
-
-@author: arsca
-"""
-
 import unittest
 from app import app, db
-from models import User, Product
 from werkzeug.security import generate_password_hash
 
 class ApiTestCase(unittest.TestCase):
@@ -15,17 +7,13 @@ class ApiTestCase(unittest.TestCase):
         self.app = app.test_client()
         db.create_all()
 
+        # Création d'un utilisateur de test
         hashed_password = generate_password_hash('password', method='sha256')
         user = User(username='testuser', password=hashed_password)
         db.session.add(user)
-
-        product1 = Product(name='Product1', price=10.99)
-        product2 = Product(name='Product2', price=15.99)
-        db.session.add(product1)
-        db.session.add(product2)
-
         db.session.commit()
 
+        # Authentification et récupération du token
         response = self.app.post('/login', json={'username': 'testuser', 'password': 'password'})
         self.token = response.get_json()['token']
 
@@ -33,10 +21,23 @@ class ApiTestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_get_products(self):
-        response = self.app.get('/products', headers={'x-access-tokens': self.token})
+    def test_get_data_animov_ch(self):
+        # Tester la route get_data_animov_ch avec les paramètres requis
+        response = self.app.get('/get_data_animov_ch', headers={'x-access-tokens': self.token}, query_string={
+            'sources': '1,2',  # Exemple de sources valides
+            'with_images': 'False',
+            'with_detect': 'False',
+            'with_stats': 'True',
+            'with_global_stats': 'True'
+        })
+
+        # Vérifier que la requête est réussie
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.get_json()), 2)
+
+        # Vérifier que la réponse contient les champs attendus
+        json_data = response.get_json()
+        self.assertIn('_general_stats', json_data)
+        self.assertIn('_send_date', json_data)
 
 if __name__ == '__main__':
     unittest.main()
